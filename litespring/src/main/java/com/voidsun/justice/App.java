e18017774d2f9b5e9b773d95d8ace273a0d2c14b
voidsun <voidsun@126.com>
2016-05-11 09:52:43 +0800

    init commit

---
+++ litespring/src/main/java/com/voidsun/justice/App.java
@@ -0,0 +1,13 @@
+package com.voidsun.justice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
